84550d55239131000010091dc45261c13a8871bb
Vadde <arvadde@deloitte.com>
2022-11-23 15:03:24 +0530

    first commit

---
+++ src/main/java/com/example/ModFsDemo/ModFsDemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.ModFsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ModFsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModFsDemoApplication.class, args);
+	}
+
+	@GetMapping("/add/{a}/{b}")
+	public String add(@PathVariable("a") int a, @PathVariable("b") int b) {
+		return String.format("Result = %s!", a + b);
+	}
+
+	@GetMapping("/substract/{a}/{b}")
+	public String substract(@PathVariable("a") int a, @PathVariable("b") int b) {
+		return String.format("Result = %s!", a - b);
+	}
+
+	@GetMapping("/divide/{a}/{b}")
+	public String sbtract(@PathVariable("a") int a, @PathVariable("b") int b) {
+		return String.format("Result = %s!", a / b);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/com/example/ModFsDemo/ModFsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ModFsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModFsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
