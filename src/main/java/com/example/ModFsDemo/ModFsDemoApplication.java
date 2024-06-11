31903767919878c3ec5d98f5ed3abf1df2509904
subhamroy5 <aravindvadde@gmail.com>
2024-06-11 22:11:40 +0530

    Added ThemwLeafe for HTML

--- src/main/java/com/example/ModFsDemo/ModFsDemoApplication.java
+++ src/main/java/com/example/ModFsDemo/ModFsDemoApplication.java
@@ -2,13 +2,15 @@ package com.example.ModFsDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class ModFsDemoApplication {
 
 	public static void main(String[] args) {
@@ -31,8 +33,9 @@ public class ModFsDemoApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format(" Hello %s!", name);
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "Hello";
 	}
 
 }
